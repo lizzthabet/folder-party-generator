29bd24034dcac93789d207ad4330b1f4876c88a1
lizz thabet <lizzthabet@gmail.com>
2024-02-19 21:15:23 -0500

    add furniture directory

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ type FileData = {
 const CURRENT_DIRECTORY = '.'
 const FOLDER_ENV_VAR = 'PARTY_FOLDER'
 const FILES_TO_IGNORE = new Set(['.DS_Store'])
+const FURNITURE_FOLDER = 'furniture'
+const OUTPUT_FILENAME = "index.html"
 const HTML_TITLE = "welcome to a place on my computer i've created just for you"
 
 function getDirFromEnv(e: NodeJS.ProcessEnv): string {
@@ -22,17 +24,43 @@ function getDirFromEnv(e: NodeJS.ProcessEnv): string {
 }
 
 function listFilesInDir(dir: string): string[] {
-  const files = readdirSync(dir, { encoding: 'utf-8', recursive: true })
-  return filterIgnoredFiles(files)
+  return readdirSync(dir, { encoding: 'utf-8', recursive: true })
 }
 
-function filterIgnoredFiles(filenames: string[]): string[] {
-  return filenames.filter(fn => !FILES_TO_IGNORE.has(fn))
+type TemplateInput = {
+  files: FileData[]
+  furniture: FileData[]
+  existingIndex?: FileData
+  // maybe option to add to index or replace or rename
+}
+
+function sortFiles(files: FileData[]): TemplateInput {
+  const input: TemplateInput = {
+    files: [],
+    furniture: [],
+  }
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i]
+    if (FILES_TO_IGNORE.has(file.parsed.name)) {
+      continue
+    }
+
+    if (file.parsed.dir === FURNITURE_FOLDER) {
+      input.furniture.push(file)
+    } else if (file.path === OUTPUT_FILENAME) {
+      input.existingIndex = file
+    } else {
+      input.files.push(file)
+    }
+  }
+
+  return input
 }
 
 function template(files: string[]): string {
   const data: FileData[] = files.map(f => ({ path: f, parsed: parse(f) }))
-  return generateTemplate(data)
+  const templateInput = sortFiles(data)
+  return generateTemplate(templateInput)
 }
 
 (function main() {
@@ -43,7 +71,8 @@ function template(files: string[]): string {
     const files = listFilesInDir(directory)
     console.log(`> > found ${files.length} files to add to folder party`)
     // TODO: make sure not to overwrite files or get confirmation?
-    writeFileSync(`${directory}/index.html`, template(files), { encoding: 'utf-8' })
+    writeFileSync(`${directory}/${OUTPUT_FILENAME}`, template(files), { encoding: 'utf-8' })
+    console.log(`> > creating folder party website file: ${directory}/${OUTPUT_FILENAME}`)
   } catch (err) {
     console.error("folder party creation failed:", err)
     process.exit(1)
@@ -51,15 +80,15 @@ function template(files: string[]): string {
 })()
 
 // Template logic * ~ * ~ *
-function generateTemplate(data: FileData[]): string {
-  return createHtmlDocument(data)
+function generateTemplate(input: TemplateInput): string {
+  return createHtmlDocument(input)
 }
 
-function createHtmlDocument(data: FileData[]): string {
+function createHtmlDocument(input: TemplateInput): string {
   return `<!DOCTYPE html>
 <html lang="en">
 ${createHead()}
-${createBody(data)}
+${createBody(input)}
 </html>`
 }
 
@@ -77,26 +106,208 @@ ${createScript()}
 function createStyle(): string {
   return `
     <style>
-      html { background-color: pink; }
+      html {
+        background-color: #d3d3d3;
+      }
+
+      .nav-link {
+        font-size: 1.5rem;
+        color: white;
+        text-shadow: 5px 5px 5px #333333;
+      }
+
+      /* dialogs should manage their own display */
+      [data-draggable]:not(dialog) {
+        display: inline-block;
+      }
+
+      [data-draggable] {
+        user-select: none;
+        margin: 0;
+        padding: 1rem;
+      }
+
+      [data-draggable]:hover {
+        cursor: move;
+      }
+
+      header {
+        padding: 1rem;
+        z-index: 1;
+      }
+
+      header > h1 {
+        font-family: monospace;
+        font-style: italic;
+        color: blue;
+        text-shadow: 5px 5px 5px rgb(51, 51, 51, 0.50);
+      }
+
+      menu {
+        list-style: none;
+        position: absolute;
+        top: 0;
+        right: 0;
+        margin: 8px;
+      }
+
+      button.filename {
+        background-color: #d2d2d2;
+        border: none;
+        border-radius: 8px;
+        box-shadow: 5px 5px 5px rgb(51, 51, 51, 0.75);
+        color: blue;
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 1rem;
+        max-width: 250px;
+        padding: 5px;
+        overflow-wrap: anywhere;
+        text-align: center;
+        text-decoration: underline;
+        width: max-content;
+      }
+
+      button.filename:focus {
+        background-color: purple;
+        color: white;
+        outline: none;
+        text-decoration: none;
+      }
+
+      button.close-fileviewer {
+        background-color: unset;
+        border: none;
+        color: #333333;
+        font-family: monospace;
+        float: right;
+        margin: 0.5rem -0.5rem -0.5rem 0.5rem;
+        text-transform: uppercase;
+      }
+
+      button.close-fileviewer:active,
+      button.close-fileviewer:focus {
+        background-color: purple;
+        border-radius: 5px;
+        color: white;
+        outline: none;
+      }
+
+      dialog.fileviewer[open] {
+        background-color: #dfdfdf;
+        box-shadow: 5px 5px 5px rgb(51, 51, 51, 0.75);
+        border-radius: 8px;
+        border: 2px dotted #333333;
+        z-index: 2;
+      }
     </style>`
 }
 
 function createScript(): string {
   return `
-    <script>console.log("TODO")</script>`
+    <script>
+    function positionInPlace(element) {
+      const { top, left } = element.getBoundingClientRect()
+      const { scrollX, scrollY } = window
+      setTopLeft(element, top + scrollY, left + scrollX)
+    }
+
+    function setTopLeft(element, top, left) {
+      element.style.setProperty("top", top + "px")
+      element.style.setProperty("left", left + "px")
+    }
+
+    function setPositionAbsolute(element) {
+      element.style.setProperty("position", "absolute")
+    }
+
+    function onMouseDown(event) {
+      event.stopPropagation()
+      const { currentTarget, target, pageX, pageY, clientX, clientY } = event
+      const { left, top } = currentTarget.getBoundingClientRect()
+      const shiftX = clientX - left
+      const shiftY = clientY - top
+
+      function moveTo(pageX, pageY) {
+        const newLeft = pageX - shiftX + "px"
+        const newTop = pageY - shiftY + "px"
+        currentTarget.style.setProperty("left", newLeft)
+        currentTarget.style.setProperty("top", newTop)
+      }
+
+      function onMouseMove(event) {
+        moveTo(event.pageX, event.pageY)
+      }
+
+      function onMouseUp() {
+        document.removeEventListener("mousemove", onMouseMove)
+        document.removeEventListener("mouseup", onMouseUp)
+      }
+
+      document.addEventListener("mousemove", onMouseMove)
+      document.addEventListener("mouseup", onMouseUp)
+    }
+
+    const DOUBLE_CLICK = 2
+    function onDoubleClick(e) {
+      if (e.detail === DOUBLE_CLICK) {
+        const dialogId = e.currentTarget.getAttribute("aria-controls")
+        if (!dialogId) {
+          return
+        }
+        const dialog = document.getElementById(dialogId)
+        if (!dialog) {
+          return
+        }
+        if (dialog.open === false) {
+          const { top, left, height, width } = e.currentTarget.getBoundingClientRect()
+          const { scrollX, scrollY } = window
+          setTopLeft(dialog, top + height + scrollY + 10, left + scrollX)
+          setPositionAbsolute(dialog)
+          dialog.show()
+        }
+      }
+    }
+
+    window.addEventListener("load", (event) => {
+      const elements = document.querySelectorAll("[data-draggable]")
+      // Position each element absolutely so document flow isn't changed
+      // when one element is moved
+      elements.forEach(positionInPlace)
+      elements.forEach(setPositionAbsolute)
+
+      // Add drag-to-move functionality
+      elements.forEach((ele) =>
+        ele.addEventListener("mousedown", onMouseDown)
+      )
+
+      // Add double-click listener to open up fileviewer
+      const fileviewers = document.querySelectorAll("[data-fileviewer]")
+      fileviewers.forEach((ele) => {
+        ele.addEventListener("click", onDoubleClick)
+      })
+    })
+    </script>`
 }
 
-function createBody(data: FileData[]): string {
+function createBody(input: TemplateInput): string {
   return `
   <body>
-    <main>${data.map((file) => {
+    <main>${input.files.map((file) => {
         return `
       ${createButton(file)}
       ${createDialog(file)}`
       }).join("\n")}
-      ${createFurniture()}
+      ${createFurniture(input.furniture)}
     </main>
   </body>`
+}
+
+function isFolder(file: FileData) {
+  return file.parsed.ext === ""
+}
+
+function displayName(file: FileData) {
+  return isFolder(file) ? `${file.path}/` : file.path
 }
 
 function createButton(file: FileData): string {
@@ -106,21 +317,30 @@ function createButton(file: FileData): string {
         aria-controls="${file.path}"
         data-fileviewer
         data-draggable>
-        ${file.path}
+        ${displayName(file)}
       </button>`
     }
 
 function createDialog(file: FileData): string {
+  const dialogContent = isFolder(file) ? `<pre>${displayName(file)} folder</pre>` :  `<object class="file" data="${file.path}" draggable="false"></object>`
   return `<dialog id="${file.path}" class="fileviewer" data-draggable>
-        <object class="file" data="${file.path}"></object>
+        ${dialogContent}
+        <form method="dialog"><button class="close-fileviewer">close</button></form>
       </dialog>`
 }
 
-function createFurniture() {
-  return `<p>TODO: furniture</p>`
+function createFurniture(furniture: FileData[]) {
+  return `<section aria-label="furniture">
+        ${furniture.map(item => {
+          return `<img src="${item.path}" draggable="false" data-draggable />`
+        }).join("\n")}
+      </section>`
 }
 
-// Button properties?
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function randomInt(min: number, max: number): number {
+  return Math.round(Math.random() * (max - min) + min)
+}
 
 // Nice-to-haves:
 // furniture folder
